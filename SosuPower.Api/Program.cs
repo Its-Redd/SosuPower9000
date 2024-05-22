4f5ef385602d2340a7f447a01aaf35188d319de0
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-05-22 13:52:12 +0200

    Changed line 16 in program.cs

--- SosuPower.Api/Program.cs
+++ SosuPower.Api/Program.cs
@@ -13,8 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IRepository<SosuPower.Entities.Task>, Repository<SosuPower.Entities.Task>>();
-
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
