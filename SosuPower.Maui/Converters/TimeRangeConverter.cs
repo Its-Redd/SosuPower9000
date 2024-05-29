6f9fb5e76540aebceb4def0746722bd7f7df6226
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-05-29 11:28:20 +0200

    Create TimeRangeConverter

---
+++ SosuPower.Maui/Converters/TimeRangeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace SosuPower.Maui.Converters
+{
+    public class TimeRangeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2)
+            {
+                throw new ArgumentException("TimeRangeConverter requires two values");
+            }
+
+            if (values[0] == null || values[1] == null)
+            {
+                return null;
+            }
+
+            if (values[0] is DateTime && values[1] is DateTime)
+            {
+                DateTime start = (DateTime)values[0];
+                DateTime end = (DateTime)values[1];
+
+                return $"{start.ToString("t")} - {end.ToString("t")}";
+            }
+
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
