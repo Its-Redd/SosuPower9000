82bea9eb319cea81768863d6ad763f6f6432d6f8
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-05-29 09:59:03 +0200

    Create BaseViewModel

---
+++ SosuPower.Maui/viewmodels/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SosuPower.Maui.viewmodels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        #region Fields
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(isNotBusy))]
+        private bool isBusy;
+
+        [ObservableProperty]
+        private string title;
+        #endregion
+
+        #region Constructor
+
+        public BaseViewModel()
+        {
+
+        }
+        #endregion
+
+
+        #region Properties
+        public bool isNotBusy => !isBusy;
+        #endregion
+    }
+
+}
