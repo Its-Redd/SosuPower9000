39a7cf21de820b6edef7e3ddcba09d58f1ae9391
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-05-29 09:19:33 +0200

    Add .NET MAUI Project to solution

---
+++ SosuPower.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SosuPower.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
