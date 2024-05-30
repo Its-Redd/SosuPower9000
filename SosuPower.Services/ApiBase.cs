36dbbcf56360d14285743fc3dbcd2021c701c5a6
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-05-30 09:59:55 +0200

    Probably the coolest commit to date

--- SosuPower.Services/ApiBase.cs
+++ SosuPower.Services/ApiBase.cs
@@ -1,7 +1,50 @@
-﻿namespace SosuPower.Services
+﻿using SosuPower.Entities;
+using Task = SosuPower.Entities.Task;
+
+namespace SosuPower.Services
 {
-    public class ApiBase
+    // Probably my favorite class in the project cause its so cool
+    public abstract class ApiBase
     {
+        protected Uri BaseUri;
+
+        protected ApiBase(Uri baseUri)
+        {
+            this.BaseUri = baseUri;
+        }
+
+        protected ApiBase(string uri) : this(new Uri(uri))
+        {
+
+        }
+    }
+
+    public class SosuService : ApiBase, SosuService.ISosuService
+    {
+        public SosuService(Uri baseUri) : base(baseUri)
+        {
+            // Initialize the service
+        }
+        public SosuService(string baseUri) : base(baseUri)
+        {
+            // Initialize the service
+        }
+
+        public List<Task> GetTasksOn(DateTime date, Employee employee)
+        {
+            UriBuilder uriBuilder = new UriBuilder(BaseUri);
+            uriBuilder.Path = "Task/GetTasksOn";
+            uriBuilder.Query = $"date={date}&employee={employee}";
+
+
+            return default;
+        }
+
+        public interface ISosuService
+        {
+            List<Entities.Task> GetTasksOn(DateTime date, Employee employee);
+        }
+
 
     }
 }
