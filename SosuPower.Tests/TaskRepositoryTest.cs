8d320af6a6c305dcee9787a61dd6c675ec98b26c
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-06-18 11:46:36 +0200

    Epic comment

--- SosuPower.Tests/TaskRepositoryTest.cs
+++ SosuPower.Tests/TaskRepositoryTest.cs
@@ -5,6 +5,9 @@ namespace SosuPower.Tests
         [Fact]
         public void IsEmployeeIdCorrect()
         {
+            // never actually had time to write tests for this project 
+
+
             // Arrange
 
             // Act
